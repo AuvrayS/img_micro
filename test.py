fa779faebda57ab807fc365d7dfe27ab59a15262
AuvrayS <simon.auvray@icloud.com>
2026-05-18 23:15:14 -0700

    Update test.py
    added a fail case

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import requests
 name = "eclipse"
+name2 = "something"
 
 response = requests.get(f"http://localhost:3001/image", params = {"name": name})
 
@@ -12,3 +13,15 @@ elif response.status_code == 404:
     print("Error:", response.jason()["error"])
 else:
     print("Unexpected status code:",response.status_code)
+
+response2 = requests.get(f"http://localhost:3001/image", params = {"name": name2})
+
+if response2.status_code ==200:
+    with open(f"{name2}_result.jpg", "wb") as f:
+        f.write(response2.content)
+    print(f"Successfully saved image as {name2}_result.jpg")
+
+elif response2.status_code == 404:
+    print("Error:", response2.jason()["error"])
+else:
+    print("Unexpected status code:",response2.status_code)
